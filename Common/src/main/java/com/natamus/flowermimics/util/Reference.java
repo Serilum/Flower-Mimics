5e0905d687135deca1e7030b0731b50252d7bd5f
Rick South <rick@natamus.com>
2023-12-12 10:11:12 +0100

    Flower Mimics source code for 1.20.1.

---
+++ Common/src/main/java/com/natamus/flowermimics/util/Reference.java
@@ -0,0 +1,8 @@
+package com.natamus.flowermimics.util;
+
+public class Reference {
+	public static final String MOD_ID = "flowermimics";
+	public static final String NAME = "Flower Mimics";
+	public static final String VERSION = "1.5";
+	public static final String ACCEPTED_VERSIONS = "[1.20.1]";
+}

84dbb0a7c5094f4b35f0acf97f1a70e1fd60c07b
Rick South <rick@natamus.com>
2023-12-12 10:11:02 +0100

    Flower Mimics source code for 1.18.2.

---
+++ Common/src/main/java/com/natamus/flowermimics/data/Variables.java
@@ -0,0 +1,9 @@
+package com.natamus.flowermimics.data;
+
+import com.mojang.logging.LogUtils;
+import org.slf4j.Logger;
+
+public class Variables {
+	public static final Logger logger = LogUtils.getLogger();
+	public static boolean processedMimicConfig = false;
+}

81405f418fccd8d4574bcc2876dd605cf23143d2
Rick South <rick@natamus.com>
2024-06-14 05:31:13 +0200

    Flower Mimics source code for 1.21.0.

---
+++ Common/src/main/java/com/natamus/flowermimics/mixin/EntityMixin.java
@@ -0,0 +1,19 @@
+package com.natamus.flowermimics.mixin;
+
+import net.minecraft.server.level.ServerLevel;
+import net.minecraft.world.entity.Entity;
+import net.minecraft.world.entity.LightningBolt;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(value = Entity.class, priority = 1001)
+public class EntityMixin {
+	@Inject(method = "thunderHit(Lnet/minecraft/server/level/ServerLevel;Lnet/minecraft/world/entity/LightningBolt;)V", at = @At(value = "HEAD"), cancellable = true)
+	public void thunderHit(ServerLevel serverLevel, LightningBolt lightningBolt, CallbackInfo ci) {
+		if (lightningBolt.getTags().contains("visualonly")) {
+			ci.cancel();
+		}
+	}
+}

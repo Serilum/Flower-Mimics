877f7283f7379ace7fdf4b97892dc8eeb52d90f5
Rick South <rick@natamus.com>
2024-07-28 07:32:41 +0200

    (v2.0) Added Bundle support. The mod can now be enabled/disabled via bundle configs. [+1]

--- NeoForge/src/main/java/com/natamus/flowermimics/ModNeoForge.java
+++ NeoForge/src/main/java/com/natamus/flowermimics/ModNeoForge.java
@@ -1,6 +1,7 @@
 package com.natamus.flowermimics;
 
 import com.natamus.collective.check.RegisterMod;
+import com.natamus.collective.check.ShouldLoadCheck;
 import com.natamus.flowermimics.neoforge.config.IntegrateNeoForgeConfig;
 import com.natamus.flowermimics.neoforge.events.NeoForgeMimicEvent;
 import com.natamus.flowermimics.util.Reference;
@@ -14,6 +15,10 @@ import net.neoforged.fml.event.lifecycle.FMLLoadCompleteEvent;
 public class ModNeoForge {
 	
 	public ModNeoForge(IEventBus modEventBus) {
+		if (!ShouldLoadCheck.shouldLoad(Reference.MOD_ID)) {
+			return;
+		}
+
 		modEventBus.addListener(this::loadComplete);
 
 		setGlobalConstants();
